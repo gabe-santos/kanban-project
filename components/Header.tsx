e211e4f9db256d1f2895c145eac42e6fbf17163f
Gabe <28gabe@gmail.com>
2024-02-01 10:22:11 -0800

    formatting

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,19 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Header({ children }: { children?: React.ReactNode }) {
-	return (
-		<div className='flex'>
-			<div className='w-[300px] h-24 gap-2 hover:cursor-pointer border-r-2 border-b-2 border-black font-[1000]'>
-				<Link
-					className='text-7xl h-full w-full overflow-hidden text-center flex items-center justify-center'
-					href='/'>
-					kanvas
-				</Link>
-			</div>
-			<div className='w-full h-24 border-b-2 border-black flex-1 flex items-center justify-between'>
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex">
+      <div className="h-24 w-[300px] gap-2 border-b-2 border-r-2 border-black font-[1000] hover:cursor-pointer">
+        <Link
+          className="flex h-full w-full items-center justify-center overflow-hidden text-center text-7xl"
+          href="/"
+        >
+          kanvas
+        </Link>
+      </div>
+      <div className="flex h-24 w-full flex-1 items-center justify-between border-b-2 border-black">
+        {children}
+      </div>
+    </div>
+  );
 }

f0e13e0ed2c42d3bef31e393ad59b1fede2fefbc
Gabe <28gabe@gmail.com>
2024-05-20 15:57:26 -0700

    fixes invalid default export children

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,10 +1,7 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
 import Header from "@/components/Header";
 import UserBoardDemo from "./UserBoardDemo";
 
-export default function Demo({ children }: { children: React.ReactNode }) {
+export default function Demo() {
   return (
     <div className="h-screen">
       <Header></Header>
